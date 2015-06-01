efdda826fa805eb9fb47fdfbb058bb80b2dbb46f
FrancisHutchings <Microsoft@FrancisHutchings.co.uk>
2015-06-01 18:27:19 +0100

    DB addition

--- JavaLearn/src/lottopicker/lottoPicker.java
+++ JavaLearn/src/lottopicker/lottoPicker.java
@@ -1,67 +1,103 @@
 
 /*
-* To change this license header, choose License Headers in Project Properties.
-* To change this template file, choose Tools | Templates
-* and open the template in the editor.
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package lottopicker;
 
-//~--- JDK imports ------------------------------------------------------------
-
+import java.io.PrintStream;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 import java.util.Random;
+import java.util.Scanner;
 
-/**
- *
- * @author frank.hutchings
- */
 public class lottoPicker {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        System.out.print(Arrays.toString(generateRandomNumbs()));
+        Boolean runThis = Boolean.valueOf(true);
+        while (runThis.booleanValue()) {
+            Integer menu_choice = getMenuItem();
+            switch (menu_choice.intValue()) {
+                case 0:
+                    runThis = Boolean.valueOf(false);
+                    break;
+                case 1:
+                    System.out.println(Arrays.toString(generateRandomNumbs()));
+                    break;
+                case 2:
+                    System.out.println("Choice 2!");
+                    break;
+                case 3:
+                    System.out.println("Choice 3!");
+                    break;
+                default:
+                    System.out.println("What??!!");
+            }
+            System.out.println("===============================\n");
+        }
     }
 
-    /**
-     * 1 to 49
-     */
     private static Integer[] generateRandomNumbs() {
-        Integer intArray[]      = new Integer[] {
-            0, 0, 0, 0, 0, 0
-        };
-        Integer tempInt         = 0;
-        Integer tempIntFind     = 1;
-        Random  randomGenerator = new Random();
+        Integer[] intArray = {Integer.valueOf(0), Integer.valueOf(0), Integer.valueOf(0), Integer.valueOf(0), Integer.valueOf(0), Integer.valueOf(0)};
+        Integer tempInt = Integer.valueOf(0);
+        Integer tempIntFind = Integer.valueOf(1);
+        Random randomGenerator = new Random();
 
-        tempIntFind = Arrays.asList(intArray).indexOf(0);
-
-        while (tempIntFind > -1) {
-            tempInt    = randomGenerator.nextInt(49);
-
+        tempIntFind = Integer.valueOf(Arrays.asList(intArray).indexOf(Integer.valueOf(0)));
+        while (tempIntFind.intValue() > -1) {
+            tempInt = Integer.valueOf(randomGenerator.nextInt(49));
             if (!numbExists(intArray, tempInt)) {
-                intArray[tempIntFind] = tempInt;
+                intArray[tempIntFind.intValue()] = tempInt;
             }
-
             Arrays.sort(intArray);
-            tempIntFind = Arrays.asList(intArray).indexOf(0);
+            tempIntFind = Integer.valueOf(Arrays.asList(intArray).indexOf(Integer.valueOf(0)));
         }
-
         return intArray;
     }
 
     private static boolean numbExists(Integer[] numbArray, Integer numbIn) {
         boolean returnVal = false;
-
-        for (int x = 1; x < numbArray.length; ++x) {
+        for (int x = 1; x < numbArray.length; x++) {
             if (Objects.equals(numbArray[x], numbIn)) {
                 returnVal = true;
                 break;
             }
         }
-
         return returnVal;
+    }
+
+    private static Integer getMenuItem() {
+        Scanner user_input = new Scanner(System.in);
+        Integer userChoice = Integer.valueOf(0);
+        int loopIdx = 1;
+        List<String> results = new ArrayList();
+        ConnectMSSQLServer myDbTest = new ConnectMSSQLServer();
+        myDbTest.displayDbProperties();
+        try {
+            ResultSet rs = myDbTest.viewTable("SELECT [menu_item] FROM [dbo].[menu_list]");
+            while (rs.next()) {
+                results.add(rs.getString(1));
+            }
+            myDbTest.closeConnection();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("Please make a selection from the menu - \n-------------------------------------\n");
+        System.out.println("    0 Exit prog");
+        for (int x = 0; x < results.size(); x++) {
+            System.out.println("    " + loopIdx + ": " + (String) results.get(x));
+            loopIdx++;
+        }
+        System.out.println("-------------------------------------");
+        try {
+            userChoice = Integer.valueOf(Integer.parseInt(user_input.next()));
+        } catch (NumberFormatException e) {
+            userChoice = Integer.valueOf(-1);
+        }
+        return userChoice;
     }
 }
