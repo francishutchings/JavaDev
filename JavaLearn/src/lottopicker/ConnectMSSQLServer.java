663e296380b06f1e8b9c288e0e52167db1d796b1
FrancisHutchings <Microsoft@FrancisHutchings.co.uk>
2015-06-01 19:47:38 +0100

    DB addition

--- JavaLearn/src/lottopicker/ConnectMSSQLServer.java
+++ JavaLearn/src/lottopicker/ConnectMSSQLServer.java
@@ -21,7 +21,7 @@ public class ConnectMSSQLServer {
     private final String portNumber = "1433";
     private final String databaseName = "dev";
     private final String userName = "sa";
-    private final String password = "DBConnect";
+    private final String password = "MSDBConnect";
     private final String selectMethod = "cursor";
 
     private String getConnectionUrl() {
